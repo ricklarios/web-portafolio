59911346e46cc44506aca754632d31f5a6643fb9
ricklarios <81334604+ricklarios@users.noreply.github.com>
2024-11-13 11:57:26 +0100

    Add side contact

--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -1,15 +1,45 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "../styles/components/main-section.css"; // Assuming you will add some styles
 import About from "./About";
 import Contact from "./Contact";
 import Education from "./Education";
 
 const MainSection = () => {
+  const divRef = useRef(null);
+  const [isOutOfView, setIsOutOfView] = useState(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        // Actualiza el estado cuando el div sale o entra en la vista
+        setIsOutOfView(!entry.isIntersecting);
+      },
+      { threshold: 0 }
+    );
+
+    const currentDiv = divRef.current;
+    if (currentDiv) {
+      observer.observe(currentDiv);
+    }
+
+    return () => {
+      if (currentDiv) {
+        observer.unobserve(currentDiv);
+      }
+    };
+  }, []);
+
   return (
     <main className="main-section">
-      <Contact />
+      <div ref={divRef}>
+        <Contact />
+      </div>
       <About />
       <Education />
+
+      <div className={`out-of-view + ${isOutOfView ? "" : "hidden"} `}>
+        <Contact />
+      </div>
     </main>
   );
 };
