bec88d978079a8fd1d5178c38bb84a850234f5fe
ricklarios <81334604+ricklarios@users.noreply.github.com>
2024-11-13 10:03:29 +0100

    Add linkedin logo

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "../styles/components/contact.css"; // Assuming you will add some styles
 import avatar from "../assets/images/Profile new.png";
+import linkedinLogo from "../assets/images/linkedin.jpg";
 
 const Contact = () => {
   return (
@@ -11,10 +12,20 @@ const Contact = () => {
         className="contact-avatar"
         style={{ width: "100px" }}
       />
-
       <h2 className="contact-title">&lt;Contact/&gt;</h2>
       <p>Name: Ricardo Larios</p>
       <p>email: ricklarios@gmail.com</p>
+      <a
+        href="https://www.linkedin.com/in/ricardo-larios"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img
+          src={linkedinLogo}
+          alt="LinkedIn"
+          className="contact-linkedin-logo"
+        />
+      </a>
       {/* <div className="contact-email-container">
         <input
           type="text"
