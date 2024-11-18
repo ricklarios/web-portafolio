b399b147a7e9654bbfc7c9c4676061cc6ed23a5f
ricklarios <81334604+ricklarios@users.noreply.github.com>
2024-11-18 10:53:51 +0100

    Add bg image to offcanvas expanded

--- src/components/Offcanvas.jsx
+++ src/components/Offcanvas.jsx
@@ -1,9 +1,10 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Offcanvas, Button } from "react-bootstrap";
+import { Offcanvas } from "react-bootstrap";
 import React, { useState } from "react";
 import "../styles/components/offcanvas.css";
 import spotifyLogo from "../assets/images/spotify.png";
 import openAiLogo from "../assets/images/openia-crop.png";
+import offcanvasBg from "../assets/images/man-4807395_1920.jpg";
 
 function OffcanvasComponent() {
   const [show, setShow] = useState(false);
@@ -13,7 +14,7 @@ function OffcanvasComponent() {
 
   return (
     <div className="offcanvas-component">
-      <h1 className="text-center">Your Mood, My Playlist !!</h1>
+      <h1 className="offcanvas-title text-center">Your Mood, My Playlist !!</h1>
       <div className="btn btn-primary w-75" onClick={handleShow}>
         Create Playlist
       </div>
@@ -28,14 +29,39 @@ function OffcanvasComponent() {
           <img src={openAiLogo} alt="OpenAI Logo" className="openai-logo" />
         </div>
       </div>
-      <Offcanvas show={show} onHide={handleClose} placement={"end"}>
-        <Offcanvas.Header closeButton>
-          <Offcanvas.Title>Offcanvas</Offcanvas.Title>
-        </Offcanvas.Header>
-        <Offcanvas.Body>
-          Some text as placeholder. In real life you can have the elements you
-          have chosen. Like, text, images, lists, etc.
-        </Offcanvas.Body>
+      <Offcanvas
+        className="offcanvas-expand"
+        show={show}
+        onHide={handleClose}
+        placement={"end"}
+        style={{
+          backgroundImage: `url(${offcanvasBg})`,
+        }}
+      >
+        <div className="offcanvas-bg">
+          <Offcanvas.Header>
+            <Offcanvas.Title>
+              <h1 className="offcanvas-title text-center">
+                Your Mood, My Playlist !!
+              </h1>
+            </Offcanvas.Title>
+            <button
+              type="button"
+              class="btn-close btn-close-white"
+              onClick={handleClose}
+            ></button>
+          </Offcanvas.Header>
+          <Offcanvas.Body>
+            <p>
+              Let me create a playlist for you using my favorite artists. Just
+              tell me your mood and the activity you are doing and I will find a
+              series of songs that will inspire you.
+            </p>
+            <p>
+              This functionality is based on the Spotify API and the OpenAI API.
+            </p>
+          </Offcanvas.Body>
+        </div>
       </Offcanvas>
     </div>
   );
